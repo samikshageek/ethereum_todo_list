0edbf8bbd2066362716799351017865944b02c67
Samiksha Ranjan <sammypcs12@gmail.com>
2022-03-22 23:29:20 +0530

    Smart contract changes

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,22 @@ pragma solidity ^0.5.0;
 
 contract TodoList{
     uint public taskCount=0;
+
+    struct Task{
+        uint id ;
+        string content ;
+        bool completed ;
+    }
+
+    constructor() public{
+        createTask("TSLP");
+    }
+
+    mapping(uint => Task) public tasks ;
+
+    function createTask(string memory content) public{
+        taskCount++ ;
+        tasks[taskCount] = Task(taskCount , content , false) ;
+    }
+
 }
